a67652c2fe162ade697f4f096c4df02c687dca79
Doug MacMillan <github@raette.com>
2019-10-03 21:21:23 -0700

    simple change to test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
 
-function App() {
+const AppPage = styled.div`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+`
+
+const App = () => {
   return (
-    <div className="App">
+    <AppPage>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+    </AppPage>
   );
 }
 
