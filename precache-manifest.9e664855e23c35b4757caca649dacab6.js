8736cc035597659acb58ef7376ed8b464472329d
Doug MacMillan <github@raette.com>
2019-10-03 21:53:26 -0700

    Updates

---
+++ precache-manifest.9e664855e23c35b4757caca649dacab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76c9275491107794905ca8a4077cf68",
+    "url": "/build/static/index.html/index.html"
+  },
+  {
+    "revision": "d51a58f68e5eb3a8dac7",
+    "url": "/build/static/index.html/static/css/main.0c452163.chunk.css"
+  },
+  {
+    "revision": "2baa2a8ed92c9f2806b5",
+    "url": "/build/static/index.html/static/js/2.40350f00.chunk.js"
+  },
+  {
+    "revision": "d51a58f68e5eb3a8dac7",
+    "url": "/build/static/index.html/static/js/main.52f1c047.chunk.js"
+  },
+  {
+    "revision": "04bbcdc28e4c37cad7f8",
+    "url": "/build/static/index.html/static/js/runtime-main.df9f63c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/index.html/precache-manifest.f411d937a8210a4bd136459bc490ed37.js"
+  "/build/static/index.html/precache-manifest.9e664855e23c35b4757caca649dacab6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/static/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
